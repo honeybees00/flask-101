48309c9856f1f010ee63be03da5fe11a86d3b787
Christina Ortiz <ortizchristina881@gmail.com>
2026-04-16 18:12:09 -0400

    first

---
+++ App.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify,request
+from psycopg2.extras import RealDictCursor
+import database
+app=Flask(__name__)
+database.init_db()
+@app.route('/')
+def home():
+    return jsonify({'message':'API running!'} )
+@app.route('/students')
+def get_students():
+    conn= database.get_connection()
+    cur= conn.cursor()
+    cur.execute('SELECT * FROM students')
+    rows=cur.fetchall()
+    cur.close();  conn.close()
+    return jsonify(rows)
+@app.route('/students',methods=['POST'])
+def create_student():
+    data=request.get_json()
+    conn= database.get_connection()
+    cur=conn.cursor()
+    cur.execute('''
+        `INSERT INTO students(name,grade,age)`
+                `VALUES(%s,%s,%s)`,
+                (data['name'],data['grade'],data['age'])
+
+    ''')
+    conn.commit(); cur.close(); conn.close(); 
+    return jsonify({"message":'person created'}), 201
+                   
--- database.py
+++ database.py
@@ -1,1 +1,32 @@
+import psycopg2p
+import os 
+from dotenv import load_dotenv
+load_dotenv() 
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv('DB_PORT'),
+        dbname=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        sslmode=os.getenv('DB_SSLMODE'),
 
+    )
+def init_db():
+    conn =get_connection()
+    cur =conn.cursor()
+    cur.execute('''
+            CREATE TABLE IF NOT EXISTS students(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                grade VARCHAR(5) NOT NULL,
+                age INTERGER
+
+                    
+                )   
+                    
+                  '''  )
+    conn.commit()
+    cur.close()
+    conn.close()
+    print('Database ready!')
